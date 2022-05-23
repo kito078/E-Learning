daab021ede0c4096ecf2bb91fa77b2e536287ca9
kito <kitokessian07@gmail.com>
2022-05-24 00:41:31 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./*html'],
+  theme: {
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors:{
+        brightGreen: '#278a7d',
+        brightDarkGreen: '#0d3934',
+        brightYellow: '#eab308',
+        500: '#eab308',
+        gray: '#d1d2d8',
+      },
+    },
+  },
+  plugins: [],
+}
